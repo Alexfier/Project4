9719bffae88c82769c7dc8671594b7db5bcd10e5
Alexander <oldberg1999@gmail.com>
2025-02-20 15:51:56 +0300

    Здача дз

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -5,15 +5,6 @@ from src.widget import get_date, mask_account_card
 
 # Фикстура для параметризованных тестов
 @pytest.fixture
-def card_data():
-    return [
-        ("Visa Platinum 7000792289606361", "Visa Platinum 700079******6361"),
-        ("Счет 73654108430135874305", "Счет **4305"),
-        ("MasterCard 5185373029202738", "MasterCard 518537******2738"),
-        ("Счет 12345678901234567890", "Счет **7890"),
-        ("Invalid Card 1234", "Ошибка: Неверный формат номера счета или карты"),
-    ]
-
 
 # Параметризованный тест для функции mask_account_card
 @pytest.mark.parametrize(
